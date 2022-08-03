a35abc2ca44e898906e57dbdc7f399accc525c30
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-08-03 23:11:23 +0200

    secret

--- secretmanger.tf
+++ secretmanger.tf
@@ -1,6 +1,6 @@
 
 #---------------create secrets Manager -----------------
-resource "aws_secretsmanager_secret" "secrets" {
+resource "aws_secretsmanager_secret" "secrets_manager" {
   name = "secret-key"
   rotation_rules {
     automatically_after_days = 0
@@ -9,7 +9,7 @@ resource "aws_secretsmanager_secret" "secrets" {
 
 #------------------- export private ssh key to secrets managers-------------
 resource "aws_secretsmanager_secret_version" "privatekey" {
-  secret_id     = aws_secretsmanager_secret.secrets.id
+  secret_id     = aws_secretsmanager_secret.secrets_manager.id
   secret_string = tls_private_key.private_key.private_key_pem
 
 }
