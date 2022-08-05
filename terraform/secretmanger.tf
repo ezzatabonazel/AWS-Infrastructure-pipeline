f5f18ffb01dd3a95b1916ee33450147e864bea79
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-08-05 07:18:42 +0200

    RDS password

--- terraform/secretmanger.tf
+++ terraform/secretmanger.tf
@@ -1,7 +1,7 @@
 
 #---------------create secrets Manager -----------------
 resource "aws_secretsmanager_secret" "secrets_manager" {
-  name = "secretkey"
+  name = "secretskeys"
   rotation_rules {
     automatically_after_days = 0
   }
@@ -12,4 +12,22 @@ resource "aws_secretsmanager_secret_version" "privatekey" {
   secret_id     = aws_secretsmanager_secret.secrets_manager.id
   secret_string = tls_private_key.private_key.private_key_pem
 
+}
+
+
+resource "random_password" "random"{
+  length           = 16
+  special          = false
+}
+
+resource "aws_secretsmanager_secret" "password" {
+  name = "rdspassword"
+  rotation_rules {
+    automatically_after_days = 0
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "password" {
+  secret_id = aws_secretsmanager_secret.password.id
+  secret_string = random_password.random.result
 }
