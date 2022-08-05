7f7d8ab4757f6225f1294e16412a79ff180a9d85
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-08-05 07:15:40 +0200

    edit app-ec2 sg

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_security_group" "demo-sg" {
   name        = "sg1"
-  description = "Allow HTTP and port 3000 traffic"
+  description = "Allow ssh and port 3000 traffic"
   vpc_id      = module.network.vpc_id
 
   ingress {
@@ -39,11 +39,94 @@ resource "aws_security_group" "demo-sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
+ ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb sg"
+  description = "alb sg"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    description = "allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "alb_sg"
+  }
+}
+
+resource "aws_security_group" "elastic-cache-sg" {
+  name        = "allow 6379"
+  description = "Allow 6379 inbound traffic"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    description = "allow 6379"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "elastic-cache"
+  }
+}
+
+
+resource "aws_security_group" "rds_sg" {
+  name        = "allow 3306"
+  description = "Allow 3306"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    description = "3306 from vpc"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "rds_sg"
   }
 }
